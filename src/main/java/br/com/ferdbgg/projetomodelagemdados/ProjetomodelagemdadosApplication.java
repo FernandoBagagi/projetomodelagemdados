a989b6b4a1b2656ec5b3bffb3c30188f96e9f644
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-02 08:58:38 -0300

    first commit

---
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/ProjetomodelagemdadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferdbgg.projetomodelagemdados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomodelagemdadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomodelagemdadosApplication.class, args);
+	}
+
+}
