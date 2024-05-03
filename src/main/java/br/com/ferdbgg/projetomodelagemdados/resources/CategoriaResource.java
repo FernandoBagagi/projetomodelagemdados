4977932f01403667bd52ef51520fd3f7b74b5933
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-03 10:40:01 -0300

    testa o Categoria.hashCode()

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/resources/CategoriaResource.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/resources/CategoriaResource.java
@@ -22,6 +22,9 @@ public class CategoriaResource {
         categorias.add(new Categoria(0, "Teste 1"));
         categorias.add(new Categoria(0, "Teste 2"));
         categorias.add(new Categoria(1, "Teste 3"));
+
+        Optional<String> hashes = categorias.stream().map(Categoria::hashCode).map(String::valueOf).reduce((s1, s2) -> s1 + ", " + s2);
+        hashes.ifPresent(System.out::println); 
         
         return categorias;
 
