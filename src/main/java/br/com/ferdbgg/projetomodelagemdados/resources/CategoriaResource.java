7303f136253d1d1c2dcd0cb2371d38e14b0cc259
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-03 10:39:26 -0300

    testa Categoria

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/resources/CategoriaResource.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/resources/CategoriaResource.java
@@ -1,8 +1,14 @@
 package br.com.ferdbgg.projetomodelagemdados.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ferdbgg.projetomodelagemdados.domain.Categoria;
 
 
 @RestController
@@ -10,8 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
     @GetMapping
-    public String testeString() {
-        return "Deu bom o teste";
+    public List<Categoria> testeString() {
+        
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(new Categoria(0, "Teste 1"));
+        categorias.add(new Categoria(0, "Teste 2"));
+        categorias.add(new Categoria(1, "Teste 3"));
+        
+        return categorias;
+
     }
 
 }
