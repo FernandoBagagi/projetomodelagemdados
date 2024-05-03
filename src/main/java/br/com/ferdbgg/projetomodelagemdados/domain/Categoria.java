6d493d1c09b19466ea93655a3f8872b14090eeab
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-03 10:41:56 -0300

    muda a implementação do hashCode e equals de Categoria

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/domain/Categoria.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/domain/Categoria.java
@@ -1,6 +1,8 @@
 package br.com.ferdbgg.projetomodelagemdados.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,14 +13,39 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @EqualsAndHashCode.Include
     private Integer id;
 
     private String nome;
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Categoria other = (Categoria) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
+    
 }
