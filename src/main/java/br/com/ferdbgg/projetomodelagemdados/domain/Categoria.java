b54bb707683550258c1330dd7d7b3f370116f9d1
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-08 09:33:44 -0300

    Muda o nome da tabela Categoria para categorias

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/domain/Categoria.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/domain/Categoria.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -18,6 +19,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "categorias")
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
