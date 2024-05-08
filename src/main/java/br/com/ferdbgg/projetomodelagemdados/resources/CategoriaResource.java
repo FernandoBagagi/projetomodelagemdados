98b6e7daf941e71eba128a2101920834d5a51142
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-08 09:34:12 -0300

    retira Autowired de CategoriaResource

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/resources/CategoriaResource.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/resources/CategoriaResource.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +21,6 @@ public class CategoriaResource {
     
     private CategoriaService categoriaService;
 
-    @Autowired
     public CategoriaResource(CategoriaService categoriaService) {
         this.categoriaService = categoriaService;
     }
