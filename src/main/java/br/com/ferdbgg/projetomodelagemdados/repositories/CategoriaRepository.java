737c42783b6b4b656f90e89ef424db972ad7db86
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-10 10:34:48 -0300

    muda o nome do pacote doman para models

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/repositories/CategoriaRepository.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/repositories/CategoriaRepository.java
@@ -3,7 +3,7 @@ package br.com.ferdbgg.projetomodelagemdados.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.ferdbgg.projetomodelagemdados.domain.Categoria;
+import br.com.ferdbgg.projetomodelagemdados.models.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
--- src/main/java/br/com/ferdbgg/projetomodelagemdados/services/CategoriaService.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/services/CategoriaService.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import br.com.ferdbgg.projetomodelagemdados.domain.Categoria;
+import br.com.ferdbgg.projetomodelagemdados.models.Categoria;
 import br.com.ferdbgg.projetomodelagemdados.repositories.CategoriaRepository;
 
 @Service
