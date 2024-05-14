b260a925a0515d190895546d3311fed549a288a8
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-14 09:54:40 -0300

    comeca a criar classe produto

---
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/models/entities/Produto.java
@@ -0,0 +1,55 @@
+package br.com.ferdbgg.projetomodelagemdados.models.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "produtos")
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String nome;
+
+    @Column(nullable = false, columnDefinition = "DECIMAL(15, 2) DEFAULT '0.00'")
+    @DecimalMin(value = "0.00")
+    private BigDecimal preco;
+
+    @Setter(value = AccessLevel.NONE)
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias.clear();
+        if(categorias != null) {
+            this.categorias.addAll(categorias);
+        }
+    }
+
+}
