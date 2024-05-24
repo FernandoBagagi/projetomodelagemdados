36043b93a6076569933036bcde677c4221625d5b
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-24 12:35:01 -0300

    retira import desnecessário em CidadeResource

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/resources/CidadeResource.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/resources/CidadeResource.java
@@ -2,7 +2,6 @@ package br.com.ferdbgg.projetomodelagemdados.resources;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
