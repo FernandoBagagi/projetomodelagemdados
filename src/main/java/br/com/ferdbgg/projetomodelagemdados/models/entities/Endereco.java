5739eecf3e0fc7804764c15a12e753ba95c133f6
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-05-28 12:03:46 -0300

    cria Endereco

---
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/models/entities/Endereco.java
@@ -0,0 +1,68 @@
+package br.com.ferdbgg.projetomodelagemdados.models.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "enderecos")
+public class Endereco implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String numero;
+
+    @Column(nullable = false)
+    private String complemento;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @ManyToOne
+    @JoinColumn(name = "idCidade")
+    private Cidade cidade;
+
+    @ManyToOne
+    @JoinColumn(name = "idCliente")
+    private Cliente cliente;
+
+    public Endereco(String logradouro, String numero, String complemento, String bairro, String cep, Cidade cidade, Cliente cliente) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.cliente = cliente;
+    }
+        
+}
