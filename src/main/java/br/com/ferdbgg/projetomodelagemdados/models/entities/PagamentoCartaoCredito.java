6d215fe54fd0044c3f4634e49547a7ad5b0cceed
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-06-11 10:49:46 -0300

    faz anotações Todo em PagamentoCartaoCredito

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/models/entities/PagamentoCartaoCredito.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/models/entities/PagamentoCartaoCredito.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 @Table(name = "pagamentoscartaocredito")
 public class PagamentoCartaoCredito extends Pagamento {
 
-    private String numeroCartaoCredito;
+    private String numeroCartaoCredito; //Criar restrição do formato xxxx xxxx xxxx xxxx
     
     public PagamentoCartaoCredito(StatusPagamentoEnum status, Pedido pedido, String numeroCartaoCredito) {
         super(status, pedido);
