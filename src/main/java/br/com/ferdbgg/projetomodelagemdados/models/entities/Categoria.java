84af75319d67a405023ca5f1926c2c2a823b3483
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-06-12 10:38:08 -0300

    Ajeita os TODO de Categoria

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/models/entities/Categoria.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/models/entities/Categoria.java
@@ -2,11 +2,13 @@ package br.com.ferdbgg.projetomodelagemdados.models.entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -15,13 +17,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -31,15 +31,15 @@ public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    //TODO: colcoar anotações do banco
+    @Column(length = 255, nullable = false)
     private String nome;
 
     @Setter(value = AccessLevel.NONE)
-    @JsonBackReference //TODO: Trocar por Jsonignore
     @ManyToMany(mappedBy = "categorias", fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<Produto> produtos = new ArrayList<>();
 
     public Categoria(Integer id, String nome) {
@@ -47,14 +47,21 @@ public class Categoria implements Serializable {
         this.nome = nome;
     }
 
-    public void setProdutos(List<Produto> produtos) {
+    public void setProdutos(Collection<Produto> produtos) {
         this.produtos.clear();
-        if(produtos != null) {
+        if (produtos != null) {
             this.produtos.addAll(produtos);
         }
     }
 
-    //TODO: pensar em manter somente o do lombok ou fazer isso em todos
+    // Essa classe implementa o equals e o hashCode dessa mandeira para caso o id
+    // seja o mesmo o equals verificar se todos os campos são iguais mesmo, nesse
+    // caso o hash pode ser igual mas o equals pode ser diferente. Por conceito isso
+    // pode acontecer mas na prática o ideal é que o hashCode seja único e coincida
+    // com o equals. Portanto, como isso é um código de estudo vai ficar essa
+    // anotação dessa possibilidade mas na prática vai ser usado o equals e hashCode
+    // do lombok usando somente o id
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -74,6 +81,11 @@ public class Categoria implements Serializable {
                 return false;
         } else if (!nome.equals(other.nome))
             return false;
+        if (produtos == null) {
+            if (other.produtos != null)
+                return false;
+        } else if (!produtos.equals(other.produtos))
+            return false;
         return true;
     }
 
@@ -81,5 +93,5 @@ public class Categoria implements Serializable {
     public int hashCode() {
         return Objects.hash(this.getId());
     }
-    
+
 }
