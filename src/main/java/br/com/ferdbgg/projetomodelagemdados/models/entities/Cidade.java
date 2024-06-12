dcfd139620b33486e164e69c1b2cf60b01aa0329
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-06-12 10:39:51 -0300

    tira AllArgs e identa Cidade

--- src/main/java/br/com/ferdbgg/projetomodelagemdados/models/entities/Cidade.java
+++ src/main/java/br/com/ferdbgg/projetomodelagemdados/models/entities/Cidade.java
@@ -13,14 +13,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -31,7 +29,7 @@ public class Cidade implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Integer id;
 
